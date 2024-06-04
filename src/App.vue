1c26e801bd29f7f460853accab9b408cc5f72f04
MattiaUsai <usai.mattia.05.04.2003@gmail.com>
2024-06-04 20:08:14 +0200

    added search

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <script>
 import titleComp from "./components/title.vue";
+import searchComp from "./components/searchComp.vue";
+import cardCharacterComp from "./components/cardCharacterComp.vue";
 export default {
   data() {},
   components: {
     titleComp,
+    searchComp,
+    cardCharacterComp,
   },
 };
 </script>
 
 <template>
   <titleComp></titleComp>
+  <searchComp></searchComp>
+  <cardCharacterComp></cardCharacterComp>
 </template>
---
+++ src/components/searchComp.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "search",
+};
+</script>
+<template>
+  <div class="searchBar">
+    <input type="text" />
+    <select name="" id="">
+      <option value="">ciao</option>
+      <option value="">ciao</option>
+      <option value="">ciao</option>
+      <option value="">ciao</option>
+      <option value="">ciao</option>
+    </select>
+    <button>Search</button>
+    <button>Reset</button>
+  </div>
+</template>
+<style></style>
